d80cdb2ab28ff05e13e1efe84d56678f401c8d5f
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-07-26 11:20:17 +0700

    create User model

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const app = require('./app')
-const dbConnect = require('./config/db')
-
+ 
 const PORT = process.env.DB_PORT || 3001
 
-dbConnect() 
 
 app.listen(PORT, () => {
   console.log(`success to listen at port ${PORT}`);
