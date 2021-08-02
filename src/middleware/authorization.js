856dfd41953ebd5482c96a4c0ea440b1868c79c4
rudikurniawan99 <rudikurniawan501@gmail.com>
2021-08-02 10:33:20 +0700

    success to create authorization for user

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -2,13 +2,14 @@ const jwt = require('jsonwebtoken')
 const { jwt_secret } = require('../config/config')
 
 const authorize = (req, res, next) => {
-  const activeUser = jwt.verify(req.token, jwt_secret)
+  const { token } = req.headers
+
+  const user = jwt.verify(token, jwt_secret)
   
-  if(!activeUser){
+  if(!user){
     throw new Error('please login first')
   }
   next()
-  console.log('middleware authorize jalan')
 }
 
 module.exports = {
